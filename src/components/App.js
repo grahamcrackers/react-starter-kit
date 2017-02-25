c7c20c79bbbe2b9134f3c993cf4b3cd14f828d0d
Graham Rogers <grogers@Grahams-MacBook-Air.local>
2017-02-25 01:00:41 -0600

    adding basic pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { Link, IndexLink } from 'react-router';
 
-export default class App extends React.Component {
-    render(){
-        return(
-            <div style={{textAlign: 'center'}}>
-                <h1>Hello World!</h1>
-            </div>
-        );
-    }
+// This is a class-based component because the current
+// version of hot reloading won't hot reload a stateless
+// component at the top-level.
+class App extends React.Component {
+  render() {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <IndexLink to="/">Home</IndexLink>
+        {' | '}
+        <Link to="/about">About</Link>
+        <br/>
+        {this.props.children}
+      </div>
+    );
+  }
 }
+
+App.propTypes = {
+  children: PropTypes.element
+};
+
+export default App;
---
+++ src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>React Starter Kit</h1>
+        
+        <div>
+            <h1>Hello Graham!</h1>
+        </div>
+    </div>
+  );
+};
+
+export default HomePage;
